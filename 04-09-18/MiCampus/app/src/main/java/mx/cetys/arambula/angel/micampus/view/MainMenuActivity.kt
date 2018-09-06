27ef881690a4c70d924cbc458feacc546786b566
IvanniaEdith <ivanniaedith@gmail.com>
2018-09-06 16:19:16 -0700

    Copia de mi campus

---
+++ 04-09-18/MiCampus/app/src/main/java/mx/cetys/arambula/angel/micampus/view/MainMenuActivity.kt
@@ -0,0 +1,15 @@
+package mx.cetys.arambula.angel.micampus.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import mx.cetys.arambula.angel.micampus.R.layout.activity_main_menu
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main_menu)
+
+       // txt_logged_user.text = intent.getStringExtra("nombre")
+    }
+}
