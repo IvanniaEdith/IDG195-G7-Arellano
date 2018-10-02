dc1311e8188ef73a8670c77b76e27a12f53b1db3
IvanniaEdith <ivanniaedith@gmail.com>
2018-10-02 14:49:27 -0700

    Actividad de lo basico

--- MiCampusReact/App.js
+++ MiCampusReact/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, TextInput, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class BCetys extends Component {
+  render() {
+    let pic = {
+      uri: 'https://www.google.com.mx/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwimsJee2-jdAhUGL3wKHeYnCXcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.timeshighereducation.com%2Fworld-university-rankings%2Fcetys-universidad&psig=AOvVaw2FXsZT0J2DOT3pPxQvyKFP&ust=1538602516687466'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+AppRegistry.registerComponent('AwesomeProject', () => Cetys);
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <image style={styles.image} source={require('./src/img/portada.png')}/>
-      <image style={styles.image} source={require('./src/img/profile.png')}/>
         <Text style={{fontWeight:'bold', fontSize:18}}>Itzel Shamira Macias Castro</Text>
-        <Text style={style.Text}>IDGD</Text>
-        <Text style={style.Text}>7mo Semestre</Text>
+        <Text>IDGD</Text>
+        <Text>7mo Semestre</Text>
       </View>
     );
   }
